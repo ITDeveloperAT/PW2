d78815187f55d9268e37a6d9fbd1c4123a6d256c
fher <fher@localhost>
2018-01-29 09:12:20 -0600

    Inicio

---
+++ XBS/RP/rpx.php
@@ -0,0 +1,159 @@
+<?php
+require '../fncn/fhr.php';
+if (isset($_REQUEST['onlo'])) {
+    $d = $_REQUEST['onlo'];
+    $d = explode("##", $d);
+    $a = inic($d);
+    $cnjt = obc();
+    $cnjtin = obcin();
+    $z=1;
+    if ($a) {
+        $nom = "";
+        echo '<label>Usuario: <b style="color: blue;">' . $a . '</b></label>';
+        echo '<div class="table-wrapper">';
+        echo '<table class="alt">';
+        echo '<thead>';
+        echo '<tr>';
+        //echo '<th>Num</th>';
+        echo '<th>Pasaporte</th>';
+        echo '<th>Nombre</th>';
+        echo '<th>Zona</th>';
+        echo '<th>Correo</th>';
+        echo '<th>Posee visa</th>';
+        // echo '<th>Pasaporte</th>';
+        echo '</tr>';
+        echo '</thead>';
+        echo '<tbody>';
+        for ($i = 0; $i < count($cnjt); $i ++) {
+            echo '<tr>';
+            for ($j = 0; $j < count($cnjt[$i]) - 1; $j ++) {
+                switch ($j) {
+                    case 2:
+                        echo '<td>';
+                        if ($cnjt[$i][$j] == "0" || $cnjt[$i][$j] == "1" || $cnjt[$i][$j] == "2" || $cnjt[$i][$j] == "3" || $cnjt[$i][$j] == "4" || $cnjt[$i][$j] == "5" || $cnjt[$i][$j] == "6") {
+                            echo $cnjt[$i][$j];
+                        } else {
+                            switch ($cnjt[$i][$j]) {
+                                case 7:
+                                    echo 'UGE';
+                                    break;
+                                case 8:
+                                    echo 'FV Especializadda';
+                                    break;
+                                case 9:
+                                    echo 'Invitado Banco Agricola';
+                                    break;
+                                case 10:
+                                    echo 'Invitado ASESUISA';
+                                    break;
+                                case 11:
+                                    echo 'Invitado especial';
+                                    break;
+                            }
+                        }
+                        echo '</td>';
+                        break;
+                    default:
+                        echo '<td>';
+                        echo $cnjt[$i][$j];
+                        echo '</td>';
+                        break;
+                    case 4:
+                        if ($cnjt[$i][$j] == "N") {
+                            echo '<td>No posee</td>';
+                        } else {
+                            if ($cnjt[$i][$j] == "VMX") {
+                                echo '<td>Mexicana</td>';
+                            } else {
+                                echo '<td>Estadounidense</td>';
+                            }
+                        }
+                        break;
+                }
+            }
+            echo '</tr>';
+            $z++;
+        }
+        
+        echo '</tbody>';
+        echo '</table>';
+        echo '</div>';
+        // echo '<br>';
+        /*
+        echo '<label><b>Usuarios inscritos con inconsistencias en datos</b></label>';
+        echo '<br>';
+        $cnjtin = obcin();
+        // var_dump();
+        
+        if (count($cnjtin) > 0) {
+            echo '<div class="table-wrapper">';
+            echo '<table class="alt">';
+            echo '<thead>';
+            echo '<tr>';
+            echo '<th>Pasaporte</th>';
+            echo '<th>Zona</th>';
+            echo '<th>Correo</th>';
+            echo '</tr>';
+            echo '</thead>';
+            echo '<tbody>';
+            for ($i = 0; $i < count($cnjtin); $i ++) {
+                echo '<tr>';
+                for ($j = 0; $j < count($cnjtin[$i]); $j ++) {
+                    switch ($j) {
+                        case 1:
+                            echo '<td>';
+                            if ($cnjtin[$i][$j] == "0" || $cnjtin[$i][$j] == "1" || $cnjtin[$i][$j] == "2" || $cnjtin[$i][$j] == "3" || $cnjtin[$i][$j] == "4" || $cnjtin[$i][$j] == "5" || $cnjtin[$i][$j] == "6") {
+                                echo $cnjtin[$i][$j];
+                            } else {
+                                switch ($cnjtin[$i][$j]) {
+                                    case 7:
+                                        echo 'UGE';
+                                        break;
+                                    case 8:
+                                        echo 'FV Especializadda';
+                                        break;
+                                    case 9:
+                                        echo 'Invitado Banco Agricola';
+                                        break;
+                                    case 10:
+                                        echo 'Invitado ASESUISA';
+                                        break;
+                                    case 11:
+                                        echo 'Invitado especial';
+                                        break;
+                                }
+                            }
+                            echo '</td>';
+                            break;
+                        default:
+                            echo '<td>';
+                            echo $cnjtin[$i][$j];
+                            echo '</td>';
+                            break;
+                    }
+                }
+                echo '</tr>';
+            }
+            
+            echo '</tbody>';
+            echo '</table>';
+            echo '</div>';
+        } else {
+            echo '<label>No se encontraron datos para mostrar</label>';
+        }
+        */
+    } else {
+        echo '<label style="color: red;">Credenciales invalidas, verifique si estan correctamente escritas</label>';
+    }
+    /*
+     * No borrar
+     */
+    // Codificacion
+    // $enc = strrev(base64_encode(base64_encode(strrev(base64_encode("Miguel")))));
+    // var_dump($enc);
+    // Decodificacion
+    // $enc = base64_decode(strrev(base64_decode(base64_decode(strrev($enc)))));
+    // var_dump($enc);
+}
+
+?>
---
+++ XBS/fncn/fhr.php
@@ -0,0 +1,161 @@
+<?php
+if (! isset($_SESSION)) {
+    session_start();
+}
+$preins = precons();
+$_SESSION['uncons'] = preins($preins);
+
+function ncpais()
+{
+    $file = "pais.txt";
+    $fp = fopen($file, "r+");
+    $i = 0;
+    while (! feof($fp)) {
+        $np[$i] = fgets($fp);
+        $i ++;
+    }
+    
+    $file = "cod.txt";
+    $fp = fopen($file, "r+");
+    $i = 0;
+    while (! feof($fp)) {
+        $cp[$i] = fgets($fp);
+        $i ++;
+    }
+    for ($i = 0; $i < count($np); $i ++) {
+        $ncp[$i] = $np[$i] . "#" . $cp[$i];
+    }
+    sort($ncp);
+    return $ncp;
+}
+
+function sfr($dts)
+{
+    $c = "INSERT INTO PAX VALUES ('$dts[6]', '$dts[2]', '$dts[3]', '$dts[4]', $dts[5], '$dts[7]', '$dts[8]', '$dts[9]', '$dts[10]', '$dts[11]', '$dts[12]', '$dts[13]', '$dts[14]', '$dts[15]', '$dts[16]', '$dts[17]', '$dts[18]', '$dts[19]', '$dts[20]', '$dts[0]', $dts[1])";
+    mysqli_query($_SESSION['uncons'], $c);
+    $c = "INSERT INTO SUBS VALUES ('$dts[6]', '', 0, 0, 0, 0, 0, 0)";
+    mysqli_query($_SESSION['uncons'], $c);
+}
+
+function obc()
+{
+    $c = "SELECT NU_PASS FROM PAX ORDER BY PAX.ZONA ASC";
+    $xc = mysqli_query($_SESSION['uncons'], $c);
+    $num = mysqli_num_rows($xc);
+    for ($i = 0; $i < $num; $i ++) {
+        $rxc[$i] = mysqli_fetch_array($xc, MYSQLI_NUM)[0];
+    }
+    for ($i = 0; $i < count($rxc); $i ++) {
+        $c = "SELECT PAX.NU_PASS, CONCAT(PAX.NOM_PAX, ' ', PAX.APE_PAX) AS NOMBRE, PAX.ZONA, PAX.EMAIL_PAX, T_VISA, PAX.ZONA FROM PAX WHERE PAX.NU_PASS = '$rxc[$i]' && NOM_PAX!='Nombres según pasaporte'";
+        $aux = mysqli_query($_SESSION['uncons'], $c);
+        $rx[$i] = mysqli_fetch_array($aux, MYSQLI_NUM);
+    }
+    return $rx;
+}
+
+function obcin()
+{
+    $c = "SELECT NU_PASS FROM PAX WHERE NOM_PAX = 'Nombres según pasaporte' ORDER BY PAX.ZONA ASC";
+    $xc = mysqli_query($_SESSION['uncons'], $c);
+    $num = mysqli_num_rows($xc);
+    for ($i = 0; $i < $num; $i ++) {
+        $rxc[$i] = mysqli_fetch_array($xc, MYSQLI_NUM)[0];
+    }
+    for ($i = 0; $i < count($rxc); $i ++) {
+        $c = "SELECT PAX.NU_PASS, PAX.EMAIL_PAX, PAX.ZONA FROM PAX WHERE PAX.NU_PASS = '$rxc[$i]'";
+        $aux = mysqli_query($_SESSION['uncons'], $c);
+        $aux2 = mysqli_fetch_array($aux, MYSQLI_NUM);
+        //var_dump($aux2);
+        if ($aux2 != NULL) {
+            $rx[$i] = $aux2;
+        }
+    }
+    return $rx;
+}
+
+function inic($a)
+{
+    $a[1] = strrev(base64_encode(base64_encode(strrev(base64_encode($a[1])))));
+    $c = "SELECT COUNT(*), NOM_US, APE_US FROM HOSTS WHERE NU_USH='$a[0]' AND PSS_US='$a[1]'";
+    $xc = mysqli_query($_SESSION['uncons'], $c);
+    $rxc = mysqli_fetch_array($xc, MYSQLI_NUM);
+    if ($rxc[0] != "0") {
+        $r = $rxc[1] . " " . $rxc[2];
+    } else {
+        $r = false;
+    }
+    return $r;
+}
+
+// conexion con usuario de solo consulta
+function precons()
+{
+    // $srvr = 'localhost';
+    // $usdb = 'nosu';
+    // $psd = '932GypOdYb4UH4mh';
+    // $ndlb = 'HXC';
+    $srvr = 'localhost';
+    $usdb = 'r8s8q9i4_us2018';
+    $psd = '.(@K6W*6seh}';
+    $ndlb = 'r8s8q9i4_bs2018';
+    return $srvr . '#' . $usdb . '#' . $psd . '#' . $ndlb;
+}
+
+function preins($supn)
+{
+    $supn = explode("#", $supn);
+    $cnctr = mysqli_connect($supn[0], $supn[1], $supn[2], $supn[3]);
+    if ($cnctr) {
+        return $cnctr;
+    } else {
+        return false;
+    }
+}
+
+function cnus($d)
+{
+    $c = "SELECT COUNT(*) FROM PAX WHERE NU_PASS='$d'";
+    $x = mysqli_query($_SESSION['uncons'], $c);
+    $rx = mysqli_fetch_array($x)[0];
+    if ((integer) $rx > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function stateman($nu, $pa)
+{
+    switch ($pa) {
+        case 0:
+            $_SESSION['nuac'] = $nu;
+            $_SESSION['logged'] = 'yes';
+            $dir = 'Location:XBS/';
+            //$dir = 'Location: /XBS/';
+            var_dump($dir);
+            echo header($dir);
+            break;
+        
+        case 1:
+            if (cnus($nu)) {
+                $_SESSION['nuac'] = $nu;
+                $_SESSION['logged'] = 'yes';
+                //$dir = 'Location:XBS/';
+                $dir = 'Location: http://' . $_SERVER['HTTP_HOST'] . '/unidosesmejor2018/XBS/';
+                echo header($dir);
+            } else {
+                echo 'Pasaporte invalido, verifique nuevamente';
+            }
+            break;
+    }
+}
+
+function nelentry()
+{
+    $_SESSION = array();
+    session_destroy();
+    $dir = 'Location: http://' . $_SERVER['HTTP_HOST'] . '/unidosesmejor2018';
+    echo header($dir);
+}
+
+?>
